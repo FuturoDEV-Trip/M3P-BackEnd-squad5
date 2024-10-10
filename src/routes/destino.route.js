df97ddd794eabf966e2c2e265126f15c12f88e11
Lucas Lino <lucaslino.martins@hotmail.com>
2024-10-10 20:06:14 -0300

    Foi feito uma nova rota em destinos para upload da imagem

--- src/routes/destino.route.js
+++ src/routes/destino.route.js
@@ -12,4 +12,19 @@ destinoRoutes.get('/:id', auth, DestinoController.listarEspecifico)
 destinoRoutes.put('/:id', auth, DestinoController.alterarDestino)
 destinoRoutes.delete('/:id', auth, DestinoController.excluirDestino)
 
+
+destinoRoutes.post("/upload-image", async (req, res) => {
+    try {
+      return res.json({
+        error: false,
+        mensagem: "Upload realizado com sucesso"
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error: true,
+        mensagem: "Erro ao realizar upload",
+      });
+    }
+  });
+
 module.exports = destinoRoutes
