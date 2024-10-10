58f7aeab6dc514a4bc5febb11c90d7aca6287f0a
Lucas Lino <lucaslino.martins@hotmail.com>
2024-10-10 20:56:55 -0300

    Atulizado o middleware

--- src/middleware/uploadImagem.js
+++ src/middleware/uploadImagem.js
@@ -1,21 +1,25 @@
 const multer = require('multer');
+const path = require('path');
 
-module.exports(multer({
-    storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, '../upload/img')
-        },
-        filename: (req, file, cb) => {
-            cb(null, Date.now().toString + "_" + file.originalname)
-        },
-
-    }),
-    fileFilter: (req, file, cb) => {
-        const extensaoImg = ['image/png', 'image/jpg', 'image/jpeg'].find
-            (fomatoAceito => fomatoAceito == file.mimetype)
-        if (extensaoImg) {
-            return cb(null, true)
-        }
-        return cb(null, false)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../upload/img')); 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now().toString() + "_" + file.originalname); 
     }
-}))
+});
+
+const fileFilter = (req, file, cb) => {
+    const extensaoImg = ['image/png', 'image/jpg', 'image/jpeg'].find(
+        formatoAceito => formatoAceito === file.mimetype
+    );
+    if (extensaoImg) {
+        return cb(null, true);
+    }
+    return cb(null, false);
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload; 
