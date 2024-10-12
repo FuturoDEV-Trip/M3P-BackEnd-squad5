a5585646ff03dda374644ea28bafbcf1ca74509a
Lucas Lino <lucaslino.martins@hotmail.com>
2024-10-11 21:08:30 -0300

    configurações do multer e aws aplicadas.

--- src/routes/img.route.js
+++ src/routes/img.route.js
@@ -1,20 +1,23 @@
-const { Router } = require('express')
+const { Router } = require('express');
+const multer = require('multer');
+const UploadImagesService = require('../service/UploadImagesService'); 
+const multerConfig = require('../config/multer');
 
-const imgRoutes = new Router();
-const UploadImagesService = require('../service/UploadImagesService')
+const imgRoutes = Router();
+const upload = multer(multerConfig);
 
-
-imgRoutes.post('/', async (req, res) => {
-    const UploadImagesService = new UploadImagesService();
-
-    await UploadImagesService.execute();
-    return response.send();
-
-})
+imgRoutes.post('/a', upload.single('file'), async (req, res) => {
+    try {
+        const uploadImagesService = new UploadImagesService(); 
+        await uploadImagesService.execute(req.file); 
+        return res.send('Arquivo enviado com sucesso!');
+    } catch (error) {
+        return res.status(500).send('Erro ao enviar o arquivo');
+    }
+});
 
 imgRoutes.delete('/:filename', async (req, res) => {
+    // Lógica para deletar o arquivo
+});
 
-})
-
-module.exports = imgRoutes
-
+module.exports = imgRoutes;
