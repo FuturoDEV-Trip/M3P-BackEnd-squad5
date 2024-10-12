31e63c71d0829785bb00782899a6ea864e3ceae7
Lucas Lino <lucaslino.martins@hotmail.com>
2024-10-11 22:19:02 -0300

    Delete e upload atualizados

--- src/service/UploadImagesService.js
+++ src/service/UploadImagesService.js
@@ -1,8 +1,10 @@
+const S3Storage = require('../utils/S3Storage');
 
 class UploadImagesService {
-    async execute() {
-
+    async execute(file) {
+        const s3Storage = new S3Storage();
+        await s3Storage.saveFile(file.filename); // Aqui, você está usando file.filename
     }
 }
 
-module.exports = UploadImagesService
+module.exports = UploadImagesService;
