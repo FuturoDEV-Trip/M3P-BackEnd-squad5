920cfa63727f74815992a63ab16294dd44996b31
bolaguerreiro <bolamg@gmail.com>
2024-10-13 19:56:18 -0300

    Configuração Acesso remoto

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -7,11 +7,17 @@ module.exports = {
   username: process.env.USERNAMEDB,
   password: process.env.PASSWORDDB,
   database: process.env.DATABASE, 
-  port: process.env.PORT 
-};
-/* dialectOptions: {
-  ssl: {
-      rejectUnauthorized: true,
-      require: true
+  port: process.env.PORT, 
+  dialectOptions: {
+    ssl: {
+        rejectUnauthorized: true,
+        require: true
+    }
+  },
+  pool:{
+    max: 5,
+    min: 0,
+    acquire:30000,
+    idle:10000
   }
-}, */
+};
