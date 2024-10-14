912df1f7632936514b4574d5a5c191ee89c5ffe6
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-10-14 15:43:19 -0300

    Realizando teste com outra chamada de api para cep

--- src/service/buscarEndereco.js
+++ src/service/buscarEndereco.js
@@ -1,35 +1,62 @@
+// const axios = require("axios");
+
+// async function buscarEndereco(cep) {
+//   try {
+//     const responseViaCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+//     if (responseViaCep.data.erro) {
+//       throw new Error("CEP inválido");
+//     }
+//     const { logradouro, bairro, localidade, uf } = responseViaCep.data;
+//     const enderecoCompleto = `${logradouro}, ${bairro}, ${localidade} - ${uf}`;
+
+//     const responseOpenStreetMap = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${logradouro},${bairro},${localidade},${uf}`);
+
+//     let coordenadas = {};
+//     if (responseOpenStreetMap.data && responseOpenStreetMap.data.length > 0) {
+//       coordenadas = {
+//         latitude: responseOpenStreetMap.data[0].lat,
+//         longitude: responseOpenStreetMap.data[0].lon
+//       };
+//     }
+
+//     return { enderecoCompleto, coordenadas, localidade };
+
+//   } catch (error) {
+//     throw new Error("Erro ao buscar o endereço pelo CEP");
+//   }
+// }
+
+// module.exports = { buscarEndereco };
+
 const axios = require("axios");
 
 async function buscarEndereco(cep) {
-    try {
+  try {
+    // Usando a API da AwesomeAPI para buscar informações do CEP
+    const response = await axios.get(`https://cep.awesomeapi.com.br/json/${cep}`);
 
-      console.log(`Buscando endereço para CEP: ${cep}`);
-        const responseViaCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        if (responseViaCep.data.erro) {
-          console.error(`CEP inválido: ${cep}`);
-            throw new Error("CEP inválido");
-        }
-        const { logradouro, bairro, localidade, uf } = responseViaCep.data;
-        const enderecoCompleto = `${logradouro}, ${bairro}, ${localidade} - ${uf}`;
-
-        const responseOpenStreetMap = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${logradouro},${bairro},${localidade},${uf}`);
-
-        let coordenadas = {};
-        if (responseOpenStreetMap.data && responseOpenStreetMap.data.length > 0) {
-          coordenadas = {
-            latitude: responseOpenStreetMap.data[0].lat,
-            longitude: responseOpenStreetMap.data[0].lon
-          };
-        } else {
-          console.error(`Erro ao buscar as coordenadas para o endereço: ${enderecoCompleto}`);
-        }
-
-        return { enderecoCompleto, coordenadas, localidade };
-
-    } catch (error) {
-      console.error('Erro ao buscar o endereço pelo CEP', error);
-      throw new Error("Erro ao buscar o endereço pelo CEP");
+    if (!response.data) {
+      throw new Error("CEP inválido");
     }
-  }
 
-  module.exports = { buscarEndereco };
+    // Extrai os dados retornados pela API
+    const { address, district, city, state, lat, lng } = response.data;
+
+    // Montando o endereço completo
+    const enderecoCompleto = `${address}, ${district}, ${city} - ${state}`;
+
+    // Coordenadas retornadas pela API, se disponíveis
+    const coordenadas = {
+      latitude: lat || null,
+      longitude: lng || null,
+    };
+
+    return { enderecoCompleto, coordenadas, localidade: city };
+
+  } catch (error) {
+    console.error("Erro ao buscar o endereço pelo CEP:", error.message);
+    throw new Error("Erro ao buscar o endereço pelo CEP");
+  }
+}
+
+module.exports = { buscarEndereco };
